139c6c61b338add276b0bfeb716c5349a6209274
goekberkusta <55788727+goekberkusta@users.noreply.github.com>
2019-12-06 08:48:41 -0800

    Update and delete methods and front end added

--- test.php
+++ test.php
@@ -2,27 +2,84 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <link rel="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>Football Club</title>
 </head>
 <body>
 <?php require_once 'process.php'; ?>
+
+<?php
+if(isset($_SESSION['message'])): ?>
+
+<div class ="alert alert-<?=$_SESSION['msg_type']?>">
+    <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    ?>
+</div>
+<?php endif ?>
+<div class="container">
+<?php
+    $mysqli = new mysqli('localhost','root','','crud') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM data");
+    //pre_r($result);
+    ?>
+<div class="row justify-content-center">
+    <table class ="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Location</th>
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        <?php
+            while($row = $result->fetch_assoc()): ?>
+        <tr>
+            <td> <?php echo $row['name'];?> </td>
+            <td> <?php echo $row['location'];?> </td>
+            <td>
+                <a href="test.php?edit=<?php echo $row['id']; ?>"
+                   class ="btn btn-info">Edit</a>
+                <a href="process.php?delete=<?php echo $row['id'];?>"
+                   class ="btn btn-danger">Delete</a>
+            </td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
+</div>
+<?php
+function pre_r($array){
+        echo'<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+?>
+
 <div class="row justify-content-center">
 <form action="process.php" method="POST">
+    <input type="hidden" name ="id" value="<?php echo $id; ?>"
     <div class="form-group">
     <label>Name</label>
-    <input type="text" name="name" class="form-control" value="Enter your name">
+    <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter your name">
     </div>
-    <div class="form-group">
+    <div class="row justify-content-center">
     <label>Location</label>
-    <input type="text" name="location" class="form-control" value="Enter your location">
+        <input type="text" name="location" class="form-control" value="<?php echo $location; ?>" placeholder="Enter your location">
      </div>
-    <div class="form-group">
-    <button type="submit" class="btn btn-primary" name="save">Save</button>
+    <div class="row justify-content-center">
+        <?php
+            if($update == true):
+        ?>
+            <button type="submit" class="btn btn-info" name="update">Update</button>
+        <?php else: ?>
+            <button type="submit" class="btn btn-primary" name="save">Save
+    <?php endif; ?>
     </div>
 </form>
+</div>
 </div>
 </body>
 </html>
